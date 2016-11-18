f30414398f015ffe7726474256b55bb41662e9b0
iAklis <chenpinwei@gmail.com>
2016-11-18 12:06:21 +0800

    disable error display

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ define('MYSQL_PASSWORD', 'aklis');
 define('MYSQL_DATABASE', 'library');
 
 
-error_reporting(1);
-ini_set('display_errors', 'On');
+error_reporting(0);
+ini_set('display_errors', 'Off');
 ini_set('allow_url_fopen', 'Off');
 ini_set('session.cookie_httponly', 1);
 session_start();
